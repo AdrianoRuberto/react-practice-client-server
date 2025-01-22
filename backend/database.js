c166fb83b63e97ce327229b50ab54b0c9627b9bd
Adriano Ruberto <aruberto@groupemutuel.ch>
2025-01-22 13:19:50 +0100

    Add database

---
+++ backend/database.js
@@ -0,0 +1,29 @@
+
+import sqljs from "sql.js";
+
+const SQL = await sqljs();
+
+// Create a database
+export const db = new SQL.Database();
+
+const init = `
+CREATE TABLE users (id int NOT NULL, firstname varchar(100) NOT NULL, lastname varchar(100) NOT NULL, type varchar(25) NOT NULL);
+INSERT INTO users VALUES (100, 'Obiwan', 'Kenobi', 'Jedi');
+INSERT INTO users VALUES (101, 'Luke', 'Skywalker', 'Jedi');
+INSERT INTO users VALUES (200, 'Boba', 'Fett', 'Bounty Hunter');
+INSERT INTO users VALUES (300, 'Darth', 'Vader', 'Sith');
+`;
+db.run(init);
+
+
+function transformToRecords(data) {
+  return data.values.map(row => data.columns.reduce((obj, column, index) => {
+    obj[column] = row[index];
+    return obj;
+  }, {}));
+}
+
+export function getAllUsers() {
+  const rows = db.exec("SELECT * FROM users");
+  return transformToRecords(rows[0]);
+}
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
-import express from 'express';
+import express from 'express'
+import {getAllUsers} from "./database.js";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!!')
 })
 
+app.get("/users", (req, res) => {
+  const users = getAllUsers();
+  res.send(users)
+});
+
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
-})
+  console.log(`backend listening on port ${port}`)
+});
